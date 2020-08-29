33c635836df41a1209b70451c6aeedd99913e6b3
AlbertYang <44716364+AlbertYang666@users.noreply.github.com>
2020-08-29 20:11:20 +0800

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,50 +1,49 @@
 /**
- * Created by Administrator on 2020-08-16.
+ * 游戏对象
+ * Created by AlbertYang on 2020-08-16.
  */
-//自调用函数---游戏对象================================================
 (function () {
 
-    var that = null;//该变量的目的就是为了保存游戏Game的实例对象-------
+    var that = null;// 该变量的目的就是为了保存游戏Game的实例对象
 
-    //游戏的构造函数
+    // 游戏对象的构造函数
     function Game(map) {
         this.food = new Food();//食物对象
         this.snake = new Snake();//小蛇对象
         this.map = map;//地图
-        that = this;//保存当前的实例对象到that变量中-----------------此时that就是this
+        that = this;//保存当前的实例对象到that变量中
     }
 
-    //初始化游戏-----可以设置小蛇和食物显示出来
+    // 为游戏对象的原型添加初始化游戏的方法---让小蛇和食物显示出来
     Game.prototype.init = function () {
-        //初始化游戏
-        //食物初始化
+        // 食物初始化
         this.food.init(this.map);
-        //小蛇初始化
+        // 小蛇初始化
         this.snake.init(this.map);
-        //调用自动移动小蛇的方法========================||调用了小蛇自动移动的方法
+        // 调用自动移动小蛇的方法
         this.runSnake(this.food, this.map);
-        //调用按键的方法
-        this.bindKey();//========================================
+        // 调用按键的方法
+        this.bindKey();
     };
 
-    //添加原型方法---设置小蛇可以自动的跑起来
+    // 为游戏对象的原型添加小蛇自动移动的方法
     Game.prototype.runSnake = function (food, map) {
 
-        //自动的去移动
+        // 通过定时器让小蛇自动的去移动
         var timeId = setInterval(function () {
-            //此时的this是window
-            //移动小蛇
+            // 此时的this是window,通过bind(that),把this变成游戏对象
+            // 移动小蛇
             this.snake.move(food, map);
-            //初始化小蛇
+            // 初始化小蛇
             this.snake.init(map);
-            //横坐标的最大值
+            // 横坐标的最大值
             var maxX = map.offsetWidth / this.snake.width;
-            //纵坐标的最大值
+            // 纵坐标的最大值
             var maxY = map.offsetHeight / this.snake.height;
-            //小蛇的头的坐标
+            // 小蛇的头的坐标
             var headX = this.snake.body[0].x;
             var headY = this.snake.body[0].y;
-            //横坐标
+            // 横坐标
             if (headX < 0 || headX >= maxX) {
                 //撞墙了,停止定时器
                 clearInterval(timeId);
@@ -56,13 +55,13 @@
                 clearInterval(timeId);
                 alert("游戏结束");
             }
-        }.bind(that), 300);
+        }.bind(that), 200);
     };
 
-    //添加原型方法---设置用户按键,改变小蛇移动的方向
+    // 添加原型方法---设置用户按键,改变小蛇移动的方向
     Game.prototype.bindKey=function () {
 
-        //获取用户的按键,改变小蛇的方向
+        // 获取用户的按键,改变小蛇的方向
         document.addEventListener("keydown",function (e) {
             //这里的this应该是触发keydown的事件的对象---document,
             //所以,这里的this就是document
@@ -76,6 +75,6 @@
         }.bind(that),false);
     };
 
-    //把Game暴露给window,外部就可以访问Game对象了
+    // 把Game暴露给window,以便外部能够使用
     window.Game = Game;
-}());
+}());// 自调用函数
